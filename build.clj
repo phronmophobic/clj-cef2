e98fcaa5ec5932e35c708dc5cb8af8fc41d94ddc
Adrian Smith <smith.adriane@gmail.com>
2023-11-05 14:40:46 -0800

    update build steps.

--- build.clj
+++ build.clj
@@ -2,8 +2,8 @@
   (:require [clojure.tools.build.api :as b]
             [clojure.string :as str]))
 
-(def lib 'com.phronemophobic/clj-cef)
-(def version "0.9.8")
+(def lib 'com.phronemophobic/clj-cef2.alpha)
+(def version "0.1-SNAPSHOT")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
@@ -12,7 +12,7 @@
   (b/delete {:path "target"}))
 
 (defn compile [_]
-  (b/javac {:src-dirs ["javasrc"]
+  #_(b/javac {:src-dirs ["javasrc"]
             :class-dir class-dir
             :basis basis
             :javac-opts ["-source" "8" "-target" "8"]}))
