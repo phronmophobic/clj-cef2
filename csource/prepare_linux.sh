ffcc166ba270abc8f6e8692983eacf6701fab67b
Adrian Smith <smith.adriane@gmail.com>
2023-11-06 06:17:01 +0000

    add missing linux deps.

--- csource/prepare_linux.sh
+++ csource/prepare_linux.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-DEBIAN_FRONTEND=noninteractive apt -y install clang xvfb libatk1.0-dev libatk-bridge2.0-dev libxkbcommon-dev libxcomposite-dev libxrandr-dev libgbm-dev libxdamage-dev
+DEBIAN_FRONTEND=noninteractive apt -y install clang xvfb libatk1.0-dev libatk-bridge2.0-dev libxkbcommon-dev libxcomposite-dev libxrandr-dev libgbm-dev libxdamage-dev libva2 libva-x11-2 libva-drm2
